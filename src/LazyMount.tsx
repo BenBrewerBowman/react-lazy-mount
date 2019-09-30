781736e2d8785a94921c549dd3ac3fc88394d9d4
benbrewerb <ben.brewer@lifeomic.com>
2019-09-30 10:42:42 -0600

    Default export LazyMount

--- src/LazyMount.tsx
+++ src/LazyMount.tsx
@@ -1,12 +1,12 @@
 import React, { ReactNode } from 'react';
 import useAfterFirstTruthy from './useAfterFirstTruthy';
 
-interface Props {
+export type LazyMountProps = {
   children: ReactNode;
   trigger: boolean;
 }
 
-const LazyMount = ({ children, trigger }: Props) => {
+const LazyMount = ({ children, trigger }: LazyMountProps) => {
   const isMounted = useAfterFirstTruthy(trigger);
 
   return <>{isMounted && children}</>;
